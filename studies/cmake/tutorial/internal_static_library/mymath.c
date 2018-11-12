44210bf08a28db3688039aa33d4fe1b6d0bdac3e
Luo Zhengrong <robin@warickmedical.com>
2018-11-12 17:26:55 +0800

    internal static library

---
+++ studies/cmake/tutorial/internal_static_library/mymath.c
@@ -0,0 +1,6 @@
+#include "mymath.h"
+
+double sum(double a, double b)
+{
+	return a + b;
+}
---
+++ studies/cmake/tutorial/internal_static_library/mymath.h
@@ -0,0 +1,6 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+double sum(double a, double b);
+
+#endif	/* MYMATH_H */
