f93a93a189d808d1c2b843c68b017a2000fedee0
Luo Zhengrong <robin@warickmedical.com>
2018-11-13 18:59:23 +0800

    random for demo

--- studies/cmake/tutorial/internal_static_library/tutorial.c
+++ studies/cmake/tutorial/internal_static_library/tutorial.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mymath.h"
 
 int main(int argc, char **argv)
 {
-	double a = 3;
-	double b = 4;
+	double a = (double)random();
+	double b = (double)random();
 	double c = sum(a, b);
 
-	printf("Hello, world!\n");
+	printf("First random is\t\t%lf\n", a);
+	printf("Second random is\t\t%lf\n", b);
+	printf("Summation is\t\t%lf\n", c);
+
 	return 0;
 }
